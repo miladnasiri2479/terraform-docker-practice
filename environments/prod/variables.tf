f1f9a7d9da57b5c2639ce204bdf85d6d4e2e4e8a
milad nasiri <miladnasiri2479@gmail.com>
2026-05-24 04:16:15 +0330

    fix: remove invalid syntax from variables.tf files and fix CI

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -5,10 +5,13 @@ variable "db_count" {
 }
 
 variable "env_prefix" { type = string }
-...
-
 variable "web_count" { type = number }
 variable "external_port_start" { type = number }
 variable "db_user" { type = string }
 variable "db_password" { type = string }
 variable "db_name" { type = string }
+
+variable "docker_host" {
+  type    = string
+  default = null
+}
